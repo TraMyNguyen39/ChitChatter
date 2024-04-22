4e30f947030fad624079ffbb2c75149603bb091a
TraMyNguyen39 <tram8319@gmail.com>
2024-04-22 17:28:48 +0700

    add login, signup, forget password ui

---
+++ app/src/main/java/com/midterm/chitchatter/ui/AuthenticationActivity.kt
@@ -0,0 +1,12 @@
+package com.midterm.chitchatter.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.midterm.chitchatter.R
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
